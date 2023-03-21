8916bac2bc3fe0977d3d527a02512d023bf0a93c
Jack Y. Araz <jackaraz@gmail.com>
2023-03-21 23:00:55 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", mode="r", encoding="UTF-8") as f:
+    requirements = f.read()
+requirements = [x for x in requirements.split("\n") if x != ""]
+
+with open("src/spey_pyhf/_version.py", mode="r", encoding="UTF-8") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
+setup(
+    name="spey-pyhf",
+    version=version,
+    description=("pyhf plugin for spey interface"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SpeysideHEP/spey-pyhf",
+    project_urls={
+        "Bug Tracker": "https://github.com/SpeysideHEP/spey-pyhf/issues",
+    },
+    # download_url=f"https://github.com/MadAnalysis/spey/archive/refs/tags/v{version}.tar.gz",
+    author="Jack Y. Araz",
+    author_email=("jack.araz@durham.ac.uk"),
+    license="MIT",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={"spey.plugins": ["pyhf = spey_pyhf:PyhfInterface"]},
+    install_requires=requirements,
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    extras_require={
+        "dev": ["pytest>=7.1.2", "pytest-cov>=3.0.0", "twine>=3.7.1", "wheel>=0.37.1"]},
+)
