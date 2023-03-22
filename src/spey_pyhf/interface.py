7dbf0527b5d45b7927ea0c9b5c9b5bd9be6fa667
Jack Y. Araz <jackaraz@gmail.com>
2023-03-22 09:54:52 +0100

    update docstring

--- src/spey_pyhf/interface.py
+++ src/spey_pyhf/interface.py
@@ -27,9 +27,6 @@ class PyhfInterface(BackendBase):
     universality across all platforms.
 
     :param model: contains all the information regarding the regions, yields
-    :param analysis: a unique name for the analysis (default `"__unknown_analysis__"`)
-    :param xsection: cross section value for the signal, only used to compute excluded cross section
-                     value. Default `NaN`
     :raises AssertionError: if the input type is wrong.
 
     .. code-block:: python3
