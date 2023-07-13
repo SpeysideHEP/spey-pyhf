3b54afb4f9c600492fa80d3387980542c7582b0f
Jack Y. Araz <jackaraz@gmail.com>
2023-07-13 14:02:32 +0100

    update setup

--- setup.py
+++ setup.py
@@ -10,6 +10,18 @@ requirements = [x for x in requirements.split("\n") if x != ""]
 with open("src/spey_pyhf/_version.py", mode="r", encoding="UTF-8") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
+docs = [
+    "sphinx>=6.1.3",
+    "sphinxcontrib-bibtex~=2.1",
+    "sphinx-click",
+    "sphinx_rtd_theme",
+    "nbsphinx!=0.8.8",
+    "sphinx-issues",
+    "sphinx-copybutton>=0.3.2",
+    "sphinx-togglebutton>=0.3.0",
+    "myst-parser",
+]
+
 setup(
     name="spey-pyhf",
     version=version,
@@ -20,7 +32,7 @@ setup(
     project_urls={
         "Bug Tracker": "https://github.com/SpeysideHEP/spey-pyhf/issues",
     },
-    # download_url=f"https://github.com/MadAnalysis/spey/archive/refs/tags/v{version}.tar.gz",
+    download_url=f"https://github.com/SpeysideHEP/spey-pyhf/archive/refs/tags/v{version}.tar.gz",
     author="Jack Y. Araz",
     author_email=("jack.araz@durham.ac.uk"),
     license="MIT",
@@ -44,5 +56,6 @@ setup(
     extras_require={
         "dev": ["pytest>=7.1.2", "pytest-cov>=3.0.0", "twine>=3.7.1", "wheel>=0.37.1"],
         "jax": ["jax>=0.3.35"],
+        "doc": docs,
     },
 )
