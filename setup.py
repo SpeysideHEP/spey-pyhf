66d86a959cb2be811999960a92cef6e262342cfe
Jack Y. Araz <jackaraz@gmail.com>
2023-09-02 11:38:39 +0100

    add simplify backend and bump spey version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", mode="r", encoding="utf-8") as f:
 with open("src/spey_pyhf/_version.py", mode="r", encoding="UTF-8") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
-requirements = ["pyhf==0.7.3", "spey>=0.1.0"]
+requirements = ["pyhf==0.7.3", "spey>=0.1.1"]
 
 docs = [
     "sphinx>=6.1.3",
@@ -40,6 +40,7 @@ setup(
         "spey.backend.plugins": [
             "pyhf.uncorrelated_background = spey_pyhf.interface:UncorrelatedBackground",
             "pyhf = spey_pyhf.interface:FullStatisticalModel",
+            "pyhf.simplify = spey_pyhf.simplify:Simplify",
         ]
     },
     install_requires=requirements,
